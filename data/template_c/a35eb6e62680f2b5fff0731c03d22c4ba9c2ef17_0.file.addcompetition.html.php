22d6aeb4d83809506c637d9b084402a6537dcf63
PHPerJiang <554246432@qq.com>
2018-06-12 19:44:34 +0800

    细微修改

--- data/template_c/a35eb6e62680f2b5fff0731c03d22c4ba9c2ef17_0.file.addcompetition.html.php
+++ data/template_c/a35eb6e62680f2b5fff0731c03d22c4ba9c2ef17_0.file.addcompetition.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.32, created on 2018-06-11 11:56:21
+/* Smarty version 3.1.32, created on 2018-06-12 19:41:32
   from 'D:\WAMPServer\Demo\signSystem\html\addcompetition.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.32',
-  'unifunc' => 'content_5b1df2e59e9f78_03771282',
+  'unifunc' => 'content_5b1fb16c230593_03232388',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'a35eb6e62680f2b5fff0731c03d22c4ba9c2ef17' => 
     array (
       0 => 'D:\\WAMPServer\\Demo\\signSystem\\html\\addcompetition.html',
-      1 => 1528687413,
+      1 => 1528803242,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5b1df2e59e9f78_03771282 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5b1fb16c230593_03232388 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
